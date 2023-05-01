09d508238e4bf2db50c60f97aa3910000bcddaff
Topius <34198115+Topius@users.noreply.github.com>
2023-05-01 09:53:42 +0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,41 +1,45 @@
 #!/bin/bash
 
+should_restart=true
 is_running=false
 pid=""
 
 # Set up signal handler for SIGINT (CTRL+C)
-trap 'echo "Stopping astrominer..."; kill $pid' SIGINT
+trap 'should_restart=false; echo "Stopping astrominer..."; kill "$pid"' SIGINT
 
-while true; do
+while $should_restart; do
   # Download the file from Github
   contents=$(curl -sSL "https://raw.githubusercontent.com/Topius/anrdoid-auto/main/command.txt")
-
   echo "Current contents: $contents"
   echo "Is running: $is_running"
-
+  
   # Check the contents of the file and run the appropriate script
   if [[ $contents == "Solo" ]]; then
-    if [[ $is_running == true ]]; then
-      echo "Stopping zolo.sh..."
-      kill $pid
+    # If the "Solo" command is received, stop the "auto.sh" script if it's running
+    if $is_running && [[ $prev_command != "Solo" ]]; then
+      echo "Stopping xauto.sh..."
+      pid=$(pgrep -f "astrominer")
+      kill "$pid"
       is_running=false
     fi
-    
+
     echo "Starting zolo.sh..."
     ./zolo.sh &
-    pid=$!
     is_running=true
+    prev_command="Solo"
   elif [[ $contents == "Auto" ]]; then
-    if [[ $is_running == true ]]; then
-      echo "Stopping xauto.sh..."
-      kill $pid
+    # If the "Auto" command is received, stop the "solo.sh" script if it's running
+    if $is_running && [[ $prev_command != "Auto" ]]; then
+      echo "Stopping zolo.sh..."
+      pid=$(pgrep -f "astrominer")
+      kill "$pid"
       is_running=false
     fi
     
     echo "Starting xauto.sh..."
     ./xauto.sh &
-    pid=$!
     is_running=true
+    prev_command="Auto"
   fi
 
   # Wait for 1 minute before checking again
