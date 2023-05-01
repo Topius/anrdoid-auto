dc4e8f9a312f11f8da307bbffd81acd616516d9e
Topius <34198115+Topius@users.noreply.github.com>
2023-05-01 09:32:49 +0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,49 +1,35 @@
 #!/bin/bash
 
-current_command=
-should_restart=true
-process_id=
-
 # Set up signal handler for SIGINT (CTRL+C)
-trap 'should_restart=false; echo "Stopping astrominer..."; kill $process_id' SIGINT
+trap 'echo "Stopping astrominer..."; kill $(pgrep astrominer); exit 0' SIGINT
 
-while $should_restart; do
+# Start with an empty command
+command=""
+
+while true; do
   # Download the file from Github
   contents=$(curl -sSL "https://raw.githubusercontent.com/Topius/anrdoid-auto/main/command.txt")
 
-  # Check the contents of the file and run the appropriate script
-  if [[ $contents == "Solo" && $current_command != "Solo" ]]; then
-    echo "Starting Solo mode..."
-    # Kill the previous process if it's running
-    if [[ ! -z "$process_id" ]]; then
-      kill $process_id
-      sleep 5
+  # Check if the contents of the file has changed
+  if [[ "$contents" != "$command" ]]; then
+    # Stop the previous astrominer process if it's running
+    if pgrep astrominer; then
+      echo "Stopping astrominer..."
+      kill $(pgrep astrominer)
     fi
-    # Run Solo mode
-    ./astrominer -w dero1qyxacd6a0xsxxdp5vwlf0hk585znc405wp5ga7g0vrcxzm665ysksqq5lv307 -r 192.168.5.104:10100 -p rpc &
-    process_id=$!
-    current_command="Solo"
-  elif [[ $contents == "Auto" && $current_command != "Auto" ]]; then
-    echo "Starting Auto mode..."
-    # Kill the previous process if it's running
-    if [[ ! -z "$process_id" ]]; then
-      kill $process_id
-      sleep 5
+
+    # Start the new astrominer process with the new command
+    if [[ "$contents" == "Solo" ]]; then
+      echo "Starting zolo.sh..."
+      command="Solo"
+      ./zolo.sh &
+    elif [[ "$contents" == "Auto" ]]; then
+      echo "Starting xauto.sh..."
+      command="Auto"
+      ./xauto.sh &
     fi
-    # Run Auto mode
-    ./astrominer -w dero1qyxacd6a0xsxxdp5vwlf0hk585znc405wp5ga7g0vrcxzm665ysksqq5lv307 -r dero-node.mysrv.cloud:10300 -p rpc &
-    process_id=$!
-    current_command="Auto"
-  elif [[ $contents == "Stop" ]]; then
-    echo "Stopping astrominer..."
-    # Stop the current process
-    if [[ ! -z "$process_id" ]]; then
-      kill $process_id
-      process_id=
-    fi
-    current_command=
   fi
 
-  # Wait for 1 minute before checking again
+  # Wait for 5 minutes before checking again
   sleep 60
 done
