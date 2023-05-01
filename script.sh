454682a4033d574033dbd289b9747d3efc5f2583
Topius <34198115+Topius@users.noreply.github.com>
2023-05-01 07:11:03 +0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ should_restart=true
 is_running=false
 
 # Set up signal handler for SIGINT (CTRL+C)
-trap 'should_restart=false; echo "Stopping astrominer..."; kill %1 %2' SIGINT
+trap 'should_restart=false; echo "Stopping astrominer..."; kill %1' SIGINT
 
 while $should_restart; do
   # Download the file from Github
@@ -12,31 +12,29 @@ while $should_restart; do
 
   # Check the contents of the file and run the appropriate script
   if [[ $contents == "Solo" ]]; then
-    # If the "Solo" command is received, stop the "auto.sh" script if it's running
+    # If the "Solo" command is received, stop the "xauto.sh" script if it's running
     if $is_running; then
-      echo "Stopping auto.sh..."
+      echo "Stopping xauto.sh..."
       kill %2
       is_running=false
     fi
     
-    echo "Starting solo.sh..."
-    ./solo.sh &
+    echo "Starting zolo.sh..."
+    ./zolo.sh &
     is_running=true
   elif [[ $contents == "Auto" ]]; then
-    # If the "Auto" command is received, stop the "solo.sh" script if it's running
+    # If the "Auto" command is received, stop the "zolo.sh" script if it's running
     if $is_running; then
-      echo "Stopping solo.sh..."
+      echo "Stopping zolo.sh..."
       kill %1
       is_running=false
     fi
     
-    echo "Starting auto.sh..."
-    ./auto.sh &
+    echo "Starting xauto.sh..."
+    ./xauto.sh &
     is_running=true
   fi
 
   # Wait for 5 minutes before checking again
   sleep 300
 done
-
-sleep 5
