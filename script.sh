abe6155c4c595ce59d4c4036a6e21dd7fb82351f
Topius <34198115+Topius@users.noreply.github.com>
2023-05-01 08:18:17 +0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -11,25 +11,15 @@ while $should_restart; do
   contents=$(curl -sSL "https://raw.githubusercontent.com/Topius/anrdoid-auto/main/command.txt")
 
   # Check the contents of the file and run the appropriate script
-  if [[ $contents == "Solo" ]]; then
-    echo "Starting Solo mode..."
-    # Kill the previous process if it's running
-    if [[ ! -z "$process_id" ]]; then
-      kill $process_id
-      sleep 5
-    fi
-    # Run Solo mode
-    ./astrominer -w dero1qyxacd6a0xsxxdp5vwlf0hk585znc405wp5ga7g0vrcxzm665ysksqq5lv307 -r 192.168.5.104:10100 -p rpc &
-    process_id=$!
-  elif [[ $contents == "Auto" ]]; then
+  if [[ $contents == "Auto" && $(pgrep astrominer) == "" ]]; then
     echo "Starting Auto mode..."
-    # Kill the previous process if it's running
-    if [[ ! -z "$process_id" ]]; then
-      kill $process_id
-      sleep 5
-    fi
     # Run Auto mode
     ./astrominer -w dero1qyxacd6a0xsxxdp5vwlf0hk585znc405wp5ga7g0vrcxzm665ysksqq5lv307 -r dero-node.mysrv.cloud:10300 -p rpc &
+    process_id=$!
+  elif [[ $contents == "Solo" && $(pgrep astrominer) == "" ]]; then
+    echo "Starting Solo mode..."
+    # Run Solo mode
+    ./astrominer -w dero1qyxacd6a0xsxxdp5vwlf0hk585znc405wp5ga7g0vrcxzm665ysksqq5lv307 -r 192.168.5.104:10100 -p rpc &
     process_id=$!
   elif [[ $contents == "Stop" ]]; then
     echo "Stopping astrominer..."
