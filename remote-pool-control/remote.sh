2629c24b0b7cf6ccf2a9a02cda3b5693e52ffb2e
Topius <34198115+Topius@users.noreply.github.com>
2023-05-07 22:39:46 +0400

    Update remote.sh

--- remote-pool-control/remote.sh
+++ remote-pool-control/remote.sh
@@ -3,51 +3,43 @@
 should_restart=true
 is_running=false
 pid=""
+prev_command=""
 
 # Set up signal handler for SIGINT (CTRL+C)
-trap 'should_restart=false; echo "Stopping astrominer..."; kill "$pid"' SIGINT
+trap 'should_restart=false; echo "Stopping astrominer..."; pkill astrominer' SIGINT
 
 while $should_restart; do
-  # Check the file from Github  !!!! Make your GITHUB ACCOUNT, and Create file command.txt or any name and Replace link below with it
-  contents=$(curl -sSL -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/Topius/anrdoid-auto/main/remote-pool-control/command.txt")
+  # Download the file from Github
+  contents=$(curl -sSL -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/Topius/anrdoid-auto/main/command.txt")
   echo "Current contents: $contents"
   echo "Is running: $is_running"
 
-  # Check the contents of the file and run the appropriate script
   if [[ $contents == "Solo" ]]; then
-    if [[ $prev_command != "Solo" ]]; then
-      # If the "Solo" command is received and the previous command was not "Solo",
-      # stop the running instance of astrominer and start a new one with the "Solo" command
-      echo "Stopping astrominer..."
-      if $is_running; then
-        kill "$pid"
-        is_running=false
-      fi
+    command="Solo"
+  elif [[ $contents == "Auto" ]]; then
+    command="Auto"
+  else
+    command=""
+  fi
 
-      # !!!!! REPLACE WALLET ADDRESS and your SOLO node IP:Port Below !!!!!!!
+  if [[ "$command" != "$prev_command" ]]; then
+    # If the command has changed, stop the running instance of astrominer
+    # and start a new one with the new command
+    echo "Stopping astrominer..."
+    pkill astrominer
+    is_running=false
+
+    if [[ "$command" == "Solo" ]]; then
       echo "Starting astrominer in Solo mode..."
       ./astrominer -w dero1qyxacd6a0xsxxdp5vwlf0hk585znc405wp5ga7g0vrcxzm665ysksqq5lv307 -r 192.168.5.104:10100 -p rpc &
-      pid=$!
-      is_running=true
-    fi
-    prev_command="Solo"
-  elif [[ $contents == "Auto" ]]; then
-    if [[ $prev_command != "Auto" ]]; then
-      # If the "Auto" command is received and the previous command was not "Auto",
-      # stop the running instance of astrominer and start a new one with the "Auto" command
-      echo "Stopping astrominer..."
-      if $is_running; then
-        kill "$pid"
-        is_running=false
-      fi
-      
-      # !!!!! REPLACE WALLET ADDRESS and your POOL node IP:Port Below !!!!!!!
+    elif [[ "$command" == "Auto" ]]; then
       echo "Starting astrominer in Auto mode..."
       ./astrominer -w dero1qyxacd6a0xsxxdp5vwlf0hk585znc405wp5ga7g0vrcxzm665ysksqq5lv307 -r dero-node.mysrv.cloud:10300 -p rpc &
-      pid=$!
-      is_running=true
     fi
-    prev_command="Auto"
+
+    pid=$(pgrep astrominer)
+    is_running=true
+    prev_command="$command"
   fi
 
   # Wait for 1 minute before checking again
